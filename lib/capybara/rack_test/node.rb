5d5f49b43420446029b0f6b4d4441b98f2cd2a32
Thomas Walpole <twalpole@gmail.com>
2016-08-17 13:28:13 -0700

    remove driver specific readonly checking - its handled in Node now

--- lib/capybara/rack_test/node.rb
+++ lib/capybara/rack_test/node.rb
@@ -28,11 +28,7 @@ class Capybara::RackTest::Node < Capybara::Driver::Node
     elsif input_field?
       set_input(value)
     elsif textarea?
-      if self[:readonly]
-        warn "Attempt to set readonly element with value: #{value} \n * This will raise an exception in a future version of Capybara"
-      else
-        native.content = value.to_s
-      end
+      native.content = value.to_s
     end
   end
 
@@ -181,11 +177,7 @@ private
       end
       native.remove
     else
-      if self[:readonly]
-        warn "Attempt to set readonly element with value: #{value} \n *This will raise an exception in a future version of Capybara"
-      else
-        native['value'] = value.to_s
-      end
+      native['value'] = value.to_s
     end
   end
 
