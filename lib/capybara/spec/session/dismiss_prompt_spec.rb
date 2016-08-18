7168be72a49b41381e2bc54da05f749ab125eb47
Thomas Walpole <twalpole@gmail.com>
2016-08-17 18:43:27 -0700

    update and test for css query special character escaping

--- lib/capybara/spec/session/dismiss_prompt_spec.rb
+++ lib/capybara/spec/session/dismiss_prompt_spec.rb
@@ -11,7 +11,7 @@ Capybara::SpecHelper.spec '#dismiss_prompt', :requires => [:modals] do
     expect(@session).to have_xpath("//a[@id='open-prompt' and @response='dismissed']")
   end
 
-  it "should return the message presented", twtw: true do
+  it "should return the message presented" do
     message = @session.dismiss_prompt do
       @session.click_link('Open prompt')
     end
